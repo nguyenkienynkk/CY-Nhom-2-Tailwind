d99c2e1745f2d27684d8f8f28804f6e3a6a0be34
luuthidzung <luuthidungtl@gmail.com>
2024-10-10 10:51:07 +0700

    bai_viet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/html/baiviet.html}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        'custom-text': '#356C07',
+        'footer': '#356C08',
+        'mucluc': '#0000004D'
+      },
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+      },
+      margin: {
+        '18': '4.5rem', 
+        '9.5': '2.375rem',
+        '2' :'68px',
+        '1':'38px'
+      },
+    },
+    },
   plugins: [],
+
+ 
 }
+
+
+
