dafe2871f21d8ef96f72f0d0a771a1623c117c26
nguyenkien <nguyenkienynk@gmail.com>
2024-10-10 11:38:13 +0700

    push-tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/html/baiviet.html}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        'custom-text': '#356C07',
-        'footer': '#356C08',
-        'mucluc': '#0000004D'
-      },
       fontFamily: {
-        'nunito': ['Nunito', 'sans-serif'],
-      },
-      margin: {
-        '18': '4.5rem', 
-        '9.5': '2.375rem',
-        '2' :'68px',
-        '1':'38px'
+        nunito: ['Nunito', 'sans-serif'],
       },
       boxShadow: {
         'box-shadow-home': '0px 0px 30.91px 0px rgba(0, 0, 0, 0.1)',
       },
     },
-    },
+  },
   plugins: [],
-
- 
 }
-
-
-
